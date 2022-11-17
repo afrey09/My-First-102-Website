918b2b4c02a0ecd448caad20a97ccd6d7b1aea53
afrey09 <AdrienneN.Frey@gmail.com>
2022-11-17 02:25:12 +0000

    Website modifications

---
+++ script.js
@@ -0,0 +1,24 @@
+alert ("Welcome Adventurer!!!");
+console.log("hello everybody")
+
+let username=prompt ("What is your name???")
+console.log
+
+document.write("Get ready for the experiences of a lifetime!!!")
+
+let response = prompt("Do you like the outdoors?")
+
+if (response == "yes") {
+    alert("You've come to the right place!");
+    document.write("You've come to the right place!");
+  } 
+  
+  else if (response == "no") {
+    alert("We have indoor activities too!");
+    document.write("We have indoor activities too!");
+  } 
+  
+  else {
+    alert("Let's get started...");
+    document.write("Let's get started...");
+  }
